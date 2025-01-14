c24151bf24af273aba9a14f577f3893b7a365888
Soham Nimbalkar <sohamnimbalkar07@gmail.com>
2025-01-14 22:18:54 +0530

    integrate typeorm and done basic setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { DatabaseModule } from './database/database.module';
+import { PhotoModule } from './modules/photo/photo.module';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PhotoModule, DatabaseModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
